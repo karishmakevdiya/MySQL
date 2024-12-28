f572f5f404946d5f5a81147b00cc0f21940ac7a0
karishmakevdiya <karishmakevdiya@gmail.com>
2024-12-28 19:54:42 +0530

    window functions

--- 11.window_functions.sql
+++ 11.window_functions.sql
@@ -16,7 +16,18 @@ join employee_salary sal
     ;
     
     
-    select dem.first_name, dem.last_name,gender, sum(salary) over(partition by gender)
+    select dem.first_name, dem.last_name,gender, salary,
+    sum(salary) over(partition by gender order by dem.employee_id) as rolling_total
+from employee_demographics dem 
+join employee_salary sal
+	on dem.employee_id = sal.employee_id
+    ;
+    
+    -- row_number is aggregate function
+       select dem.first_name, dem.employee_id, dem.last_name,gender, salary,
+    row_number() over(partition by gender order by salary desc) as row_num,
+    rank() over(partition by gender order by salary desc) as rank_num,
+    dense_rank() over(partition by gender order by salary desc) as dense_rank_num
 from employee_demographics dem 
 join employee_salary sal
 	on dem.employee_id = sal.employee_id
